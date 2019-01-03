6b7ba3715df1af34619a7d3f2d66ce6976c04baf
gwaredd <gwaredd@hotmail.com>
2019-01-03 13:11:00 +0000

    adding markdig

---
+++ Assets/MarkdownViewer/Editor/MarkdownViewer.cs
@@ -0,0 +1,19 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//------------------------------------------------------------------------------
+
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace MG
+{
+    public class MarkdownViewer : Editor
+    {
+        // Selection.activeObject.name
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# ref
+
+https://github.com/lunet-io/markdig
+https://www.nuget.org/packages/Markdig.Signed/
+https://github.com/RichardSlater/Markdig.SyntaxHighlighting/
+
+
