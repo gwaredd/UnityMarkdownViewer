bc4d01eb407b3b44970ffc196c987fba89bafa4e
gwaredd <gwaredd@hotmail.com>
2019-01-03 13:52:42 +0000

    added placeholder editor that detects .md files

--- Assets/MarkdownViewer/Editor/MarkdownViewer.cs
+++ Assets/MarkdownViewer/Editor/MarkdownViewer.cs
@@ -1,18 +1,35 @@
 ﻿////////////////////////////////////////////////////////////////////////////////
 //------------------------------------------------------------------------------
 
+using System;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
 
 namespace MG
 {
+    [CustomEditor( typeof( TextAsset ) )]
     public class MarkdownViewer : Editor
     {
-        // Selection.activeObject.name
+        public GUISkin Skin;
 
         public override void OnInspectorGUI()
         {
+            // get file extension
+
+            var path = AssetDatabase.GetAssetPath( target );
+            var ext  = Path.GetExtension( path );
+
+            if( ".md".Equals( ext, StringComparison.OrdinalIgnoreCase ) == false )
+            {
+                base.OnInspectorGUI();
+                return;
+            }
+
+            // do the thing ...
+
+
+            Debug.Log( "#path# " + path );
             base.OnInspectorGUI();
         }
     }
---
+++ Assets/Test/test_md.md
@@ -0,0 +1,7 @@
+# test 
+
+* list
+* more items
+* [link](https://www.google.com)
+
+etc
