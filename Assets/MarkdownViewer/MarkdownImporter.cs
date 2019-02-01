730b9e0e3f1446f5600c2625de40de38f90c3a16
Gwaredd Mountain <gwaredd@hotmail.com>
2019-02-01 15:09:23 +0000

    unity 2017 fix

--- Assets/MarkdownViewer/MarkdownImporter.cs
+++ Assets/MarkdownViewer/MarkdownImporter.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿#if UNITY_2018_1_OR_NEWER
+
+using UnityEngine;
 using UnityEditor.Experimental.AssetImporters;
 using System.IO;
 
@@ -10,9 +12,10 @@ namespace MG.MDV
         public override void OnImportAsset( AssetImportContext ctx )
         {
             var md = new TextAsset( File.ReadAllText( ctx.assetPath ) );
-
             ctx.AddObjectToAsset( "main", md );
             ctx.SetMainObject( md );
         }
     }
 }
+
+#endif
